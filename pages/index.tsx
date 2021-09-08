1e716a6b37411fd536d30ae5e1335e673a67f556
yoga-au <yogaagung.utama@gmail.com>
2021-09-08 16:08:43 +0700

    add index nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,35 @@
 import type { NextPage } from "next";
-import { Box } from "@chakra-ui/react";
+import {
+  Container,
+  Text,
+  ButtonGroup,
+  Button,
+  Center,
+  VStack,
+} from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Box>Hello from Chakra UI</Box>
-    </>
+    <Container>
+      <Text
+        as="h1"
+        fontSize="5xl"
+        align="center"
+        fontWeight="bold"
+        casing="uppercase"
+        mt="48"
+      >
+        Learning to use React Query
+      </Text>
+      <Center>
+        <ButtonGroup colorScheme="blue" variant="outline" mt="8">
+          <VStack spacing={8}>
+            <Button>Example with CoinGecko API</Button>
+            <Button>Example of Mutation in React Query</Button>
+          </VStack>
+        </ButtonGroup>
+      </Center>
+    </Container>
   );
 };
 
